61e0c0154f029ffefaa0a6184ce22ba8011dea4c
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-28 16:22:14 +0530

    Update zip_problem.py

--- zip_problem.py
+++ zip_problem.py
@@ -1,11 +1,9 @@
 l1=['A','B']
 l2=['a','b']
 l= list(zip(l1,l2))
-print(l)
-def concatenate(lst):
-    lst2=list()
-    for i in lst:
-        lst2.append(str(i).replace("', '","-"))
-    return lst2
 
-print(concatenate(l))
+finlst=list()
+for iterator1,iterator2 in zip(l1,l2):
+    finlst.append(iterator1+'-'+iterator2)
+
+print(finlst)
