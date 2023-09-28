6a378b674b5faf01d6de9e8d112d1f5a901cffa3
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-28 17:44:27 +0530

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,69 @@
+def gencubes(n):
+    for num in range(n):
+        yield num**3
+
+for x in gencubes(10):
+    print(x)
+
+
+def genfibon(n):
+    a=1
+    b=1
+    for i in range(n):
+        yield 
+        a,b = b, a+b
+
+
+for num in genfibon(10):
+    print(num)
+
+def fibon(n):
+    a=1
+    b=1
+    output = []
+    for i in range(n):
+        output.append(a)
+        a, b = b, a+b
+    return output
+
+fibon(10)
+def simple_gen():
+    for x in range(3):
+        yield x
+
+g=simple_gen()
+
+print(next(g))
+print(next(g))
+print(next(g))
+
+s='hello'
+
+
+for let in s:
+    print(let)
+
+
+
+s_iter=iter(s)
+next(s_iter)
+next(s_iter)
+next(s_iter)
+next(s_iter)
+next(s_iter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
