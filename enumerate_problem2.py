b0e05319bfd65a2142c7d834f7dde584b07e8619
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-28 16:43:06 +0530

    Update enumerate_problem2.py

--- enumerate_problem2.py
+++ enumerate_problem2.py
@@ -1,10 +1,12 @@
 lst1=[0,2,2,1,5,5,6,10]
-enm1=list(enumerate(lst1))
-finlst=list()
-for count,item in enumerate(lst1):
-    if count==item:
-        finlst.append(item)
-    else:
-        continue
 
-print(finlst)
+def ennum(lst):
+    finlst=list()
+    for count,item in enumerate(lst):
+        if count==item:
+            finlst.append(item)
+        else:
+            continue
+    return finlst
+
+print(ennum(lst1))
