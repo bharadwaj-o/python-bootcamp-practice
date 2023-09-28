28880032057d9fd34b3c054ecc4a6ead5cc26759
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-28 16:43:09 +0530

    Update zip_problem.py

--- zip_problem.py
+++ zip_problem.py
@@ -1,9 +1,11 @@
-l1=['A','B']
-l2=['a','b']
-l= list(zip(l1,l2))
+lst1=['A','B']
+lst2=['a','b']
+connector='-'
 
-finlst=list()
-for iterator1,iterator2 in zip(l1,l2):
-    finlst.append(iterator1+'-'+iterator2)
+def concatenator(l1,l2,conn):
+    finlst=list()
+    for iterator1,iterator2 in zip(l1,l2):
+        finlst.append(iterator1+conn+iterator2)
+    return finlst
 
-print(finlst)
+print(concatenator(lst1,lst2,connector))
